9062c2c2374bc08951999eb009b9d75034f4a729
Debjit Ghosh <debjit.prof@gmail.com>
2024-04-06 14:12:34 +0530

    added base logic users getting linked

--- controllers/indentifyController.js
+++ controllers/indentifyController.js
@@ -1,14 +1,42 @@
 const database = require('../database');
 
 const identifyController = async (req, res, next) => {
-  const data = await database.getAllContacts();
-  console.log('data:', data);
-  console.log('req body:', req.body);
+  const matchedContacts = await database.getContacts(req.body);
+  console.log('matchedContacts:', matchedContacts);
+  let primaryContacts = [];
+  const emails = [];
+  const phoneNumbers = [];
+  const secondaryContactIds = [];
+  if (req.body.email) {
+    emails.push(req.body.email);
+  }
+  if (req.body.phoneNumber) {
+    phoneNumbers.push(req.body.phoneNumber);
+  }
+  matchedContacts.forEach((element) => {
+    if (element.email) emails.push(element.email);
+    if (element.phoneNumber) phoneNumbers.push(element.phoneNumber);
+    if (element.linkPrecedence === 'secondary')
+      secondaryContactIds.push(element.id);
+  });
+
+  if (matchedContacts.length) {
+    primaryContacts = matchedContacts.filter(
+      (item) => item.linkPrecedence === 'primary'
+    );
+    console.log('primaryContacts', primaryContacts);
+  }
+  let linkPrecedence = 'primary';
+  let linkedId = null;
+  if (primaryContacts.length !== 0) {
+    linkPrecedence = 'secondary';
+    linkedId = primaryContacts[0].id;
+  }
   const contact = {
     phoneNumber: req.body.phoneNumber,
     email: req.body.email,
-    linkedId: null,
-    linkPrecedence: 'primary',
+    linkedId,
+    linkPrecedence,
     createdAt: new Date(Date.now())
       .toISOString()
       .slice(0, 19)
@@ -19,10 +47,18 @@ const identifyController = async (req, res, next) => {
       .replace('T', ' '),
     deletedAt: null,
   };
-  const insertData = await database.insertContact(contact);
-  console.log('insertData:', insertData);
+  const insertedContact = await database.insertContact(contact);
+  console.log('insertedContact:', insertedContact);
+  if (matchedContacts.length) {
+    secondaryContactIds.push(insertedContact.insertId);
+  }
   res.status(200).json({
-    message: 'success',
+    contact: {
+      primaryContatctId: linkedId || insertedContact.insertId,
+      emails: [...new Set(emails)], // first element being email of primary contact
+      phoneNumbers: [...new Set(phoneNumbers)], // first element being phoneNumber of primary contact
+      secondaryContactIds: secondaryContactIds, // Array of all Contact IDs that are "secondary" to the primary contact
+    },
   });
 };
 
--- database.js
+++ database.js
@@ -9,10 +9,71 @@ const pool = mysql
   })
   .promise();
 
-exports.getAllContacts = async () => {
-  const [records] = await pool.query('SELECT * FROM contacts');
-  return records;
+exports.getContacts = async ({ email, phoneNumber }) => {
+  let records = [];
+
+  try {
+    if (phoneNumber || email) {
+      if (phoneNumber && email) {
+        const [results] = await pool.query(
+          'SELECT * FROM contacts WHERE phoneNumber=? OR email=?',
+          [phoneNumber, email]
+        );
+        records = results;
+      } else if (phoneNumber) {
+        const [results] = await pool.query(
+          'SELECT * FROM contacts WHERE phoneNumber=?',
+          [phoneNumber]
+        );
+        records = results;
+      } else if (email) {
+        const [results] = await pool.query(
+          'SELECT * FROM contacts WHERE email=?',
+          [email]
+        );
+        records = results;
+      }
+    } else {
+      const [results] = await pool.query('SELECT * FROM contacts');
+      records = results;
+    }
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+  }
+
+  console.log('records', records);
+  let primaryContacts = [];
+  if (records.length) {
+    primaryContacts = records.filter(
+      (item) => item.linkPrecedence === 'primary'
+    );
+    console.log('primaryContactsDB', primaryContacts);
+  }
+
+  let primaryId;
+  if (primaryContacts.length) {
+    primaryId = primaryContacts[0].id;
+  } else {
+    if (records.length) primaryId = records[0].linkedId;
+  }
+  console.log('primaryId', primaryId);
+  if (!primaryId) return [];
+
+  let linkedRecords = [];
+  try {
+    const [results] = await pool.query(
+      'SELECT * FROM contacts WHERE linkedId=? OR id=?',
+      [primaryId, primaryId]
+    );
+    linkedRecords = results;
+  } catch (error) {
+    console.error('Error fetching contacts:', error);
+  }
+
+  console.log('linkedRecords', linkedRecords);
+  return linkedRecords;
 };
+
 exports.insertContact = async (contact) => {
   const [records] = await pool.query(
     'INSERT INTO contacts (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt, deletedAt) VALUES (?, ?, ?, ?, ?, ?, ?)',
